0790ff7e3e59d90bca15a18bf9ada2135fb4d9fc
Gashie22 <masogashie@gmail.com>
2024-04-11 10:32:57 +0200

    sorting update

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -13,7 +13,7 @@ const initialState = {
 
 export const LoginUser = createAsyncThunk("user/LoginUser", async (user, thunkAPI) => {
     try {
-        const response = await axios.post('http://localhost:5001/login', {
+        const response = await axios.post('http://143.244.178.37:5001/login', {
             email: user.email,
             password: user.password
         });
@@ -34,7 +34,7 @@ export const LoginUser = createAsyncThunk("user/LoginUser", async (user, thunkAP
 
 export const getMe = createAsyncThunk("user/getMe", async (_, thunkAPI) => {
     try {
-        const response = await axios.get('http://localhost:5001/me',
+        const response = await axios.get('http://143.244.178.37:5001/me',
         {
           headers: {
             "Content-Type": "application/json",
@@ -51,7 +51,7 @@ export const getMe = createAsyncThunk("user/getMe", async (_, thunkAPI) => {
 });
 
 export const LogOut = createAsyncThunk("user/LogOut", async () => {
-    await axios.delete('http://localhost:5001/logout');
+    await axios.delete('http://143.244.178.37:5001/logout');
 });
 
 export const authSlice = createSlice({
